ae969b33c4f3fda9c20cedb6591ecffc15174ed8
Luke Davids <lukedavids@gmail.com>
2025-04-08 11:06:15 -0500

    Altered pricing to use toFixed instead of toLocale

--- scripts/Sales.js
+++ scripts/Sales.js
@@ -7,7 +7,7 @@ export const Sales = async () => {
     
     const salesString = sales.map((obj) => {
         const totalPrice = obj.entree.price + obj.vegetable.price + obj.side.price
-        return `Receipt #${obj.id} $${totalPrice.toLocaleString()}<br>`
+        return `Receipt #${obj.id} $${totalPrice.toFixed(2)}<br>`
     })
 
     return salesHTML += salesString.join("")
